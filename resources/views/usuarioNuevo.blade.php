9d7247e348d01052b88b69f8384d27f4eafa3131
metalargento <aliagajoseignacio@gmail.com>
2022-01-24 23:07:27 -0300

    Update usuarioNuevo.blade.php

--- resources/views/usuarioNuevo.blade.php
+++ resources/views/usuarioNuevo.blade.php
@@ -22,7 +22,7 @@
     </head>
     <body>
         @csrf
-    <form action="https://proyectolaravel2022.herokuapp.com/public/usuario-nuevo" method="post">
+    <form action="https://proyecto-25watts-back.herokuapp.com//public/usuario-nuevo"  method="post">
     {{ csrf_field() }}
     <table>
         <tr><td>Nombre</td>
